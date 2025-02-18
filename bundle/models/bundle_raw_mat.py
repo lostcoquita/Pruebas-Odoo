40a30b527bfe6fbeacd8015559fba0ba53d30235
lostcoquita <143372974+lostcoquita@users.noreply.github.com>
2025-02-18 13:03:07 -0600

    Update bundle_raw_mat.py

--- bundle/models/bundle_raw_mat.py
+++ bundle/models/bundle_raw_mat.py
@@ -9,7 +9,7 @@ class BundleRawMat(models.Model):
     _description = 'Represent the component of a bundle'
 
     sequence = fields.Integer(string='sequence', default=10)
-    product_tmpl_id = fields.Many2one('product.template', string='Bundle', required=True, ondelete='cascade')
+    product_tmpl_id = fields.Many2one('product.template', string='Bundle prueba', required=True, ondelete='cascade')
     product_id = fields.Many2one('product.product', string="Product")
     quantity = fields.Float(string='Quantity', default=1.0)
     uom_id = fields.Many2one('uom.uom', string='Unit Of Measure', domain="[('category_id', '=', product_uom_category_id)]")
